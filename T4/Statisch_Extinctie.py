34337b0194a8524ad64fedaec7fad8e9703cffd5
Fleur van Balen <fleur.van.balen@mac.com>
2021-11-03 19:20:42 +0100

    T4 finished

--- T4/Statisch_Extinctie.py
+++ T4/Statisch_Extinctie.py
@@ -13,16 +13,16 @@ I_1 = np.array([0.98,    1.96,   3.94,   5.28])             # Stroom in ampere (
 sI_1 = np.array([0.0393, 0.0496, 0.0694, 0.0828])           # Fout vd stroom
 
 Delta_theta_1 = np.array([5, 5, 6, 10]) * (2/360) * np.pi   # Verschil van de hoek tot de beginpositie in radialen (rad)
-sDelta_theta_1 = np.array(4*[np.sqrt(2*(4**2))])*(2/360)*np.pi  # afleesfout is 4 graden, delta = begin - eind dus fout delta = sqrt(4^2+4^2) graden
+sDelta_theta_1 = np.array(4*[np.sqrt(2*(3**2))])*(2/360)*np.pi  # afleesfout is 4 graden, delta = begin - eind dus fout delta = sqrt(4^2+4^2) graden
 
 l_1 = np.array(4 * [10.2]) * 10**-2                         # Lengte van staaf in meter (m)
 sl_1 = np.array(4 * [0.05]) * 10**-2
 
 B_1 = 11.1 * I_1 * 10**(-3)                                 # Berekening van het B-veld in tesla (T)
-sB_1 = B_1 * (sI_1/I_1)**2
+sB_1 = B_1 * sI_1/I_1
 
 v_1 = Delta_theta_1 / (B_1*l_1)
-sv_1 = v_1 * ( (sDelta_theta_1/Delta_theta_1)**2 + (sB_1/B_1)**2 + (sl_1/l_1)**2 )
+sv_1 = v_1 * np.sqrt( (sDelta_theta_1/Delta_theta_1)**2 + (sB_1/B_1)**2 + (sl_1/l_1)**2 )
 # De fout komt eigenlijk hoger uit sinds de hoek met minimale intensiteit veel moeilijker is te bepalen dan 2 graden, dus als we voor het aflezen een fout van 4 nemen:
 
 print("")
@@ -73,7 +73,7 @@ plt.plot(curve_x, curve_y, color='red', label='Fit: $\\theta= v B \\ell$')
 
 plt.grid(True)
 plt.xlim(0, 0.07)
-plt.ylim(-0.1, 0.30)   
+plt.ylim(-0.05, 0.30)   
 plt.xlabel('$B$ (T)')
 plt.ylabel('$\\theta$ (rad)')
 plt.legend(loc="upper left")
--- T4/Statisch_FixedIntensity.py
+++ T4/Statisch_FixedIntensity.py
@@ -9,19 +9,19 @@ from fitcode import curve_fit
 ### --------------------------------
 
 I_2 = np.array([0.97, 1.97, 3.93, 5.9])                     # Stroom in ampere (A)
-sI_2 = np.array([0.0393, 0.0496, 0.0694, 0.0828])   
+sI_2 = np.array([0.0393, 0.0496, 0.0694, 0.089])   
 
 Delta_theta_2 = np.array([1, 2, 5, 7]) * (2/360) * np.pi    # 
-sDelta_theta_2 = np.array(4*[np.sqrt(2*(2**2))])*(2/360)*np.pi  # afleesfout is 2 graden, delta = begin - eind dus fout delta = sqrt(2^2+2^2) graden
+sDelta_theta_2 = np.array(4*[np.sqrt(2*(1**2))])*(2/360)*np.pi  # afleesfout is 2 graden, delta = begin - eind dus fout delta = sqrt(2^2+2^2) graden
 
 l_2 = np.array(4 * [10.2]) * 10**-2                         # Lengte van staaf in meter (m)
 sl_2 = np.array(4 * [0.05]) * 10**-2
 
 B_2 = 11.1 * I_2 * 10**(-3)                                 # Berekening van het B-veld in tesla (T)
-sB_2 = B_2 * (sI_2/I_2)**2
+sB_2 = B_2 * sI_2/I_2
 
 v_2 = Delta_theta_2 / (B_2*l_2)
-sv_2 = v_2 * ( (sDelta_theta_2/Delta_theta_2)**2 + (sB_2/B_2)**2 + (sl_2/l_2)**2 )
+sv_2 = v_2 * np.sqrt( (sDelta_theta_2/Delta_theta_2)**2 + (sB_2/B_2)**2 + (sl_2/l_2)**2 )
 # De fout komt hoger uit sinds de hoek met minimale hoek theta veel moeilijker is te bepalen dan 2 graden
 
 print("")
@@ -72,7 +72,7 @@ plt.plot(curve_x, curve_y, color='red', label='Fit: $\\theta= v B \\ell$')
 
 plt.grid(True)
 plt.xlim(0, 0.07)
-plt.ylim(-0.1, 0.30)   
+plt.ylim(-0.05, 0.30)   
 plt.xlabel('$B$ (T)')
 plt.ylabel('$\\theta$ (rad)')
 plt.legend(loc="upper left")
