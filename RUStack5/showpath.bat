77619b1e43b6e2ecf5d05953b1e5b7c0af9c534b
Fleur van Balen <fleur.van.balen@mac.com>
2021-12-07 14:27:29 +0100

    RUStack5 update

---
+++ RUStack5/showpath.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Voeg het volgende pad toe aan het PYTHONPATH in je ontwikkelomgeving:
+echo %~dp0
+echo NB: In dit venster werkt ctrl-C niet om het pad te kopiëren, selecteer in plaats daarvan de tekst, klik met rechts en kies kopiëren.
+pause
