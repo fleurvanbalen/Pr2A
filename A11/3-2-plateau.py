e7f77ccfd6de04ca792e3bb4ff8044d7d9932a30
Fleur van Balen <fleur.van.balen@mac.com>
2021-12-16 14:21:44 +0100

    3-2 done

--- A11/3-2-plateau.py
+++ A11/3-2-plateau.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv = pd.read_csv("Plateau_data.csv")
+csv = pd.read_csv("Data folder/3-2/Plateau_data.csv")
 
 voltage = csv['Voltage'].values.tolist()
 counts = csv['Counts'].values.tolist()
 
 plt.errorbar(voltage, counts, fmt='.', color='black', label='Metingen') 
-plt.axvline(x=475, color='red', linestyle='dotted', label='Start plateau')
-plt.axvline(x=775, color='red', linestyle='dotted', label='Eind plateau')
+plt.axvline(x=475, color='red', linestyle='dotted', label='Start en eind plateau')
+plt.axvline(x=675, color='red', linestyle='dotted')
 
 # plt.xscale('log')
 plt.grid(True)
-plt.xlim(275, 800)
+plt.xlim(275, 700)
 plt.ylim(0, 3000)
-plt.xlabel('$\\lambda$ (?)')
-plt.ylabel('Counts/s')
+plt.xlabel('Spanning over de GM-buis $U_{GM}$ (in V)')
+plt.ylabel('Intensiteit (in counts/s)')
 plt.legend(loc="upper left")
 
 plt.show()
