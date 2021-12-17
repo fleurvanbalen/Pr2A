4c85438e8b871763ebcc5e710f1484774c57f4b4
ThomasBaks <th.baks@protonmail.com>
2021-12-17 23:08:32 +0100

    Alles van 3-5 zonder foutenanalyse
    3-4 T2 erbij uitgewerkt
    3-5 Uitgewerkt, alleen de foutenanalyse mist nog

--- A11/3-5-Compton.py
+++ A11/3-5-Compton.py
@@ -1,20 +1,74 @@
-## Importeren
+## Importing
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import pandas as pd
 import os
 
-## Functies
-def TM(l,a,n):
-    return np.exp(-a*(l/100)**n)
+## Functions
+def Dlambda(a,n,R0,R,R1,R2):
+    return 100*(((np.log(R0-R)-np.log(R2-R))/(a))**(1/n)-((np.log(R0-R)-np.log(R1-R))/(a))**(1/n))
+
+def Lambda(a, n, R0,R,Ri):
+    return 100*((np.log(R0-R)-np.log(Ri-R))/(a))**(1/n)
+
+me = 9.1 * 10**(-31) #kg
+h  = 6.63 * 10**(-34) #J/Hz
+c  = 2.9 * 10**8
+def theory(theta):
+    return h/(me*c) * (1-np.cos(theta))
 
 ## Data
-os.chdir('/home/thomasbaks/Documents/GitHub/Pr2A/A11')
+os.chdir('/home/thomasbaks/Documents/GitHub/Pr2A/A11') #Just to be sure: Changing directories
 
-Data = pd.read_excel('Data folder/3-4/Transmission_values_no_Cu_Al.xlsx')
+#Found data
+R0 = 15.22 #mean hits with no filter
+Bg = 0.157 #mean hits with no cristal
 
-print(Data)
-nlpm = Data['n&l / pm'].values.tolist()
-T_1 = Data['T_1 / %'].values.tolist()
-T_2 = Data['T_2 / %'].values.tolist()
+    #With filters (R1 = filter on collimator, R2 = filter on GM-Tube)
+Cu_R2 = np.array([1.367,1.581])
+Cu_R1 = 2.476
+
+Al_R2 = np.array([1.350,1.624])
+Al_R1 = 4.302
+
+## Results (Calculating the values of Dlambda, lambda_before adn Lambda_after
+#Values n and n (To be copied from 3-4 Transmission.py
+a_T_1_Cu = 5.2687599841178185
+n_T_1_Cu = 2.956282083665764
+
+a_T_2_Al = 4.5251433961793435
+n_T_2_Al = 3.403781466979703
+
+Dlambda_Cu = Dlambda(a_T_1_Cu, n_T_1_Cu,R0,Bg,Cu_R1,Cu_R2[1])
+lambda_Cu_before = Lambda(a_T_1_Cu, n_T_1_Cu,R0,Bg,Cu_R1)
+lambda_Cu_after = Lambda(a_T_1_Cu, n_T_1_Cu,R0,Bg,Cu_R2[1])
+
+Dlambda_Al = Dlambda(a_T_2_Al, n_T_2_Al,R0,Bg,Al_R1,Al_R2[1])
+lambda_Al_before = Lambda(a_T_2_Al, n_T_2_Al,R0,Bg,Al_R1)
+lambda_Al_after = Lambda(a_T_2_Al, n_T_2_Al,R0,Bg,Al_R2[1])
+
+Dlambda_th = theory(145*np.pi/180)
+
+## Printing the found values
+
+print("Values Cu:")
+print("Dlamda:", Dlambda_Cu)
+print("Lambda_before:",lambda_Cu_before)
+print("Lambda after:",lambda_Cu_after)
+
+print()
+
+print("Values Al:")
+print("Dlamda:", Dlambda_Al)
+print("Lambda_before:",lambda_Al_before)
+print("Lambda after:",lambda_Al_after)
+
+print()
+
+print("Theory:")
+print("Dlamda:", Dlambda_th*10**(12))
+
+
+
+
